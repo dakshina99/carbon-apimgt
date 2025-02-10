86ef190ccbde23a646817d584a5d90f17e394b08
Savindu Dimal <dimalsavindu@gmail.com>
2025-02-10 17:09:49 +0530

    Remove organization name from organization subscription policies

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/APIMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/APIMappingUtil.java
@@ -330,7 +330,6 @@ public class APIMappingUtil {
                     apiTiersForOrganization.add(new Tier(tier));
                 }
                 organizationTiers.setOrganizationID(organizationPoliciesDTO.getOrganizationID());
-                organizationTiers.setOrganizationName(organizationPoliciesDTO.getOrganizationName());
                 organizationTiers.setTiers(apiTiersForOrganization);
                 organizationAPITiers.add(organizationTiers);
             }
@@ -2766,7 +2765,6 @@ public class APIMappingUtil {
         }
         organizationPoliciesDTO.setPolicies(tiersToReturn);
         organizationPoliciesDTO.setOrganizationID(organizationTiers.getOrganizationID());
-        organizationPoliciesDTO.setOrganizationName(organizationTiers.getOrganizationName());
         return organizationPoliciesDTO;
     }
 
